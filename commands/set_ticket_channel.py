374614aa8ae14b347dab85251aadbf8045a5bb5f
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-02-08 00:07:03 +0300

    set ticket channel command added

--- commands/set_ticket_channel.py
+++ commands/set_ticket_channel.py
@@ -3,13 +3,17 @@ from models.guild import get_or_create_guild
 import discord
 
 
-async def set_ticket_channel(ctx: discord.ext.commands.Context):
-    guild = await get_or_create_guild(ctx.guild.id)
-    channel = ctx.channel
-    category = channel.category
+async def set_ticket_channel(ctx: discord.ext.commands.Context, channel: discord.TextChannel):
+    if channel is None:
+        channel = ctx.channel
 
+    if channel.category is None:
+        await ctx.send("Channel must be in a category")
+        return
+
+    guild = await get_or_create_guild(ctx.guild.id)
     guild.ticket_channel = channel
-    guild.ticket_category = category
+    guild.ticket_category = channel.category
 
     view = Counter()
 
