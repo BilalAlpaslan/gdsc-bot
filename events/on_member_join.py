5320a18e4c1cd45e75aa492faef05651321b225c
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-02-07 07:15:44 +0300

    on member join invite code control

--- events/on_member_join.py
+++ events/on_member_join.py
@@ -1,8 +1,23 @@
 
 import discord
+from core.bot import GDSCBot
 from models.gif import get_random_gif
 from models.guild import get_or_create_guild
 from models.user import users
+
+invites = {}
+bot = GDSCBot()
+
+
+async def start_watch():
+    for guild in bot.guilds:
+        invites[guild.id] = await guild.invites()
+
+
+def find_invite_by_code(invite_list, code):
+    for inv in invite_list:
+        if inv.code == code:
+            return inv
 
 
 async def on_member_join(member: discord.Member):
@@ -16,3 +31,20 @@ async def on_member_join(member: discord.Member):
         user = users.get(member.id)
         role = member.guild.get_role(user.tags_with_id[0])
         await member.add_roles(role)
+
+    invites_before_join = invites[member.guild.id]
+    invites_after_join = await member.guild.invites()
+
+    for invite in invites_before_join:
+        if invite.uses < find_invite_by_code(invites_after_join, invite.code).uses:
+
+            print(f"Member {member.name} Joined")
+            print(f"Invite Code: {invite.code}")
+            print(f"Inviter: {invite.inviter}")
+
+            invites[member.guild.id] = invites_after_join
+            
+            if invite.code == "P43YCr6U":
+                await member.add_roles(member.guild.get_role(938796651877920859))
+
+            return
--- events/on_ready.py
+++ events/on_ready.py
@@ -1,6 +1,7 @@
 
 import discord
 from core.bot import GDSCBot
+from events.on_member_join import start_watch
 
 bot = GDSCBot()
 
@@ -8,3 +9,6 @@ bot = GDSCBot()
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
     await bot.change_presence(activity=discord.Game(name='!help'))
+
+    # for wacth new member with using invite code
+    await start_watch()
