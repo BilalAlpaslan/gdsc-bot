74dbb613a6a75dd46849cd80a54e93be9aabe2d3
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-02-13 08:13:51 +0300

    cleaned

--- cogs/example.py
+++ cogs/example.py
@@ -1,23 +1,3 @@
-
-# import discord
-
-# from discord.ext import commands
-
-
-# class Roles:
-
-#     def __init__(self, bot):
-#         self.bot = bot
-
-#     @commands.command(name='addrole', help='Add a role to a user')
-#     async def addrole(self, ctx, role: discord.Role, user: discord.Member):
-#         await user.add_roles(role)
-#         await ctx.send(f'{user.mention} has been given the {role.name} role.')
-
-
-# def setup(bot: commands.Bot):
-#     bot.add_cog(Roles(bot))
-
 from discord.ext import commands
 
 
