e3e149d5a90ba0cd751156301041ce36e6c61e84
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-02-23 12:47:14 +0300

    gifs update

--- models/gif.py
+++ models/gif.py
@@ -10,6 +10,13 @@ class Gif(BaseModel):
 gifs = [
     Gif(url="https://tenor.com/view/hello-there-private-from-penguins-of-madagascar-hi-wave-hey-there-gif-16043627", category="hello"),
     Gif(url="https://tenor.com/view/hello-there-baby-yoda-mandolorian-hello-gif-20136589", category="hello"),
+    Gif(url="https://tenor.com/view/hello-gif-20758171", category="hello"),
+    Gif(url="https://tenor.com/view/cat-cute-animals-hello-there-gif-11875188", category="hello"),
+    Gif(url="https://tenor.com/view/quby-chan-hi-wave-hello-hi-there-gif-17010845", category="hello")
+    Gif(url="https://tenor.com/view/hello-cute-cat-hi-greetings-gif-16242995", category="hello")
+    Gif(url="https://tenor.com/view/sup-hi-hey-bear-animal-gif-23962860", category="hello")
+    Gif(url="https://tenor.com/view/puppy-love-hi-dont-leave-cute-adorable-gif-17101332", category="hello"),
+    Gif(url="https://tenor.com/view/hello-dog-hi-cute-cute-dog-gif-14091311", category="hello"),
 ]
 
 
